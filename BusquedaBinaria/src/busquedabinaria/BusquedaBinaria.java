623c2d82dfbfff8991491ced025d2af6227524ef
Inaki Martin <inaki@webcrashers.com>
2016-08-17 18:46:22 -0300

    test

--- BusquedaBinaria/src/busquedabinaria/BusquedaBinaria.java
+++ BusquedaBinaria/src/busquedabinaria/BusquedaBinaria.java
@@ -10,12 +10,39 @@ package busquedabinaria;
  * @author Inaki
  */
 public class BusquedaBinaria {
+    public static boolean busquedaBinaria (int[] conjunto, int srch, int i, int f){
+        
+        //Casos base
+        ////////////////////////////////////////////
+        //Array = nulo
+        if (conjunto == null){
+            return false;
+        }
+        //Array = []
+        if ((f - i) == -1){
+            return false;
+        }
+        //Array = [1elem]
+        if ((f - i) == 0){
+            return (conjunto[i] == srch);
+        }
+        //Array = [a, b, c, d, e...]
+        int central = ((f - i) / 2) + i;
+        
+        //El buscado es el central
+        if (conjunto[central] == srch){
+            return true;
+        }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        //Recursividad
+        ////////////////////////////////////////////
+        //El buscado es menor al central
+        if (conjunto[central] > srch){
+            f = central - 1;
+            return busquedaBinaria(conjunto, srch, i, f);
+        // El buscado es mayor al central
+        } else {
+            i = central + 1;
+            return busquedaBinaria(conjunto, srch, i, f);
+        }
 }

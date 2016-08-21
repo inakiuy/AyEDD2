51fdf9ff959346728a45d3a244fb02a507659192
Javier Martin <Inaki@10.8.46.56>
2016-08-20 22:25:56 -0300

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	renamed:    UT1_PD1/src/ut1_pd1/UT1_PD1.java -> UT1_PD1/src/ejercicio1/Vector.java
    
     Changes not staged for commit:
    	modified:   UT1_PD1/nbproject/project.properties
    	modified:   UT1_PD1/src/ejercicio1/Vector.java
    
     Untracked files:
    	.gitignore
    	UT1_PD1/test/

--- UT1_PD1/src/ejercicio1/Vector.java
+++ UT1_PD1/src/ejercicio1/Vector.java
@@ -3,19 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ut1_pd1;
+package ejercicio1;
 
 /**
  *
  * @author Inaki
  */
-public class UT1_PD1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Vector {
+    
+    private final int[] vector;
+    
+    public Vector(int[] vector){
+        this.vector = vector;
     }
+    
     
 }
